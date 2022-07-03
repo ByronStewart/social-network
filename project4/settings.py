36c6582c3e1c64fac270406c36116fdeecaf0f26
Byron Stewart <byronstewart22@gmail.com>
2022-07-03 17:40:41 +1000

    fixed security vulnerability

--- project4/settings.py
+++ project4/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read.strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
