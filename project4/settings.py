72ff9f83c965bd1c8e978132b4b437547833a9d2
Byron Stewart <byronstewart22@gmail.com>
2022-09-13 17:52:02 +1000

    csrf fixes

--- project4/settings.py
+++ project4/settings.py
@@ -34,6 +34,8 @@ DEBUG = True if os.environ.get('RUNNING_ENV', default="production") == 'dev' els
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOST")]
 
 CSRF_TRUSTED_ORIGINS=[os.environ.get("CSRF_TRUSTED_ORIGINS")]
+CSRF_COOKIE_DOMAIN = ".railway.app"
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
